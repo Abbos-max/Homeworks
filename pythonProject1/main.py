8e67b6d292bdd915081bfb99e15960552e6fb92e
Abbos-max <toxtaxon74@gmail.com>
2024-07-15 16:09:08 +0500

    feat: test2

---
+++ pythonProject1/main.py
@@ -0,0 +1,727 @@
+# # # # # # #
+# # # # # # #
+# # # # # # # #print("Hello World", 1, 'salom', sep="--")
+# # # # # # # #print("Hello world", 1, 'abbos', end=" 99")
+# # # # # # #
+# # # # # # #
+# # # # # # # #age=int(input("Enter your age"))
+# # # # # # # #print(age)
+# # # # # # # #print(age+4)
+# # # # # # #
+# # # # # # # #main.pyc
+# # # # # # #
+# # # # # # # #narxi = int(input('maxsulot narxi:'))
+# # # # # # # #soni = int(input('maxsulot soni'))
+# # # # # # # #foyda = narxi * soni
+# # # # # # # #print("foyda: ", foyda)
+# # # # # # #
+# # # # # # # narxi = int(input('maxsulot narxi'))
+# # # # # # # nechta = int(input('maxsulot soni'))
+# # # # # # # foyiz = int(input('soliq'))
+# # # # # # # foyda = (narxi * nechta * (1-foyiz/100))
+# # # # # # # print("foyda", foyda)
+# # # # # # import random
+# # # # # #
+# # # # # # import math
+# # # # # #
+# # # # # # #
+# # # # # #
+# # # # # #
+# # # # # #
+# # # # # #
+# # # # # #
+# # # # # # #nechta = int(input('nechta maxsulot:'))
+# # # # # # #narxi = int(input('maxsulot narxi'))
+# # # # # # #foyda = nechta * narxi
+# # # # # # #print (foyda)
+# # # # # # #son = 10
+# # # # # # #c = 3456
+# # # # # # #c = str(c)
+# # # # # #
+# # # # # # # raqam = input('raqam')
+# # # # # # # code = (raqam [4:6])
+# # # # # # # if code == '99':
+# # # # # # #     print ('uzmobile')
+# # # # # # # else:
+# # # # # # #     print ('beeline')
+# # # # # #
+# # # # # # # narxi = 15000
+# # # # # # # soni = 12
+# # # # # # # foyda = soni * narxi
+# # # # # # #
+# # # # # # # print ( foyda)
+# # # # # #
+# # # # # # # narxi = int(input('maxsulot narxi'))
+# # # # # # # soni = int(input('maxsulot soni'))
+# # # # # # # jami = narxi * soni
+# # # # # # # soliq  = jami * 12 / 100
+# # # # # # # foyda = jami - soliq
+# # # # # # #
+# # # # # # # print (foyda)
+# # # # # #
+# # # # # #
+# # # # # # import math
+# # # # # #
+# # # # # # #print (math.factorial(5))
+# # # # # #
+# # # # # #
+# # # # # #
+# # # # # # print(random.randint(3,5))
+# # # # # #
+# # # # #
+# # # # #
+# # # # #
+# # # # # # a = 'Lorem ipsum is simply dummy text of the printing and typesetting'
+# # # # #
+# # # # # # print (a[::-1])
+# # # # # # print (a)
+# # # # # # print(type(a))
+# # # # # #
+# # # # # # print(a[0])
+# # # # # #
+# # # # # # c = 1234
+# # # # # # c = str(c)
+# # # # # # print(c[3])
+# # # # # # print (a[-1])
+# # # # #
+# # # # # # b = 'nimadir'
+# # # # # # c = a + " "+ b
+# # # # # # print (c)
+# # # # #
+# # # # # raqam = input('raqam')
+# # # # # code = (raqam [4:6])
+# # # # # if  code[4:6]=="90":
+# # # # #      print ('beeline')
+# # # # # else:
+# # # # #      print ('beeline')
+# # # # #
+# # # #
+# # # # son = int(input('a '))
+# # # # a = son // 100
+# # # # b = son % 100 // 10
+# # # # c = son % 10
+# # # # son = a + b + c
+# # # #
+# # # # print (son)
+# # #
+# # #
+# # # a = str('Adasi')
+# # #
+# # # print ('a')
+# #
+# #
+# # # a = input('Add any info')
+# # #
+# # # if a.isdigit():
+# # #     print(int(a)*2)
+# # # else:
+# # #     print (a, "son emas")
+# #
+# #
+# # # a = 'sdjak jsdkjdk'
+# # #
+# # # a = a.replace('s','k')
+# # # print (a)
+# # #
+# # # a = 5
+# # # b = 7
+# # # # a + b = 12
+# # #
+# # # print(f'a + b = {a + b}')
+# #
+# # # a = 'uzunligi'
+# # #
+# # # if len(a) > 10:
+# # #     print("kattasi")
+# # # else:
+# # #     print("kichkina")
+# #
+# #
+# # # a = int(input('sonni kiriting'))
+# # #
+# # # if a % 3==0:
+# # #     print ('teskari',int(str(a)[::-1]))
+# # # else:
+# # #     Print ('notogri')
+# # #     print(teskari)
+# #
+# #
+# # # n = int(inpu('son')) 10
+# # #
+# # # for i in range(1, n+1):
+# # # if  i % 2 ==0:
+# # #     print(i)
+# # #
+# # #
+# # #
+# # # # Y = 1
+# # # #
+# # # # for i in range (12, 45, 2):
+# # # #     Y= Y * i
+# # # # print(Y)
+# # #
+# # # n=int(input('son: '))
+# # # y=0
+# # #
+# # # while n > 0:
+# # #     a = n % 10
+# # #     y = y+n
+# # #     n = n//10
+# # # print(y)
+# #
+# #
+# # # py_list = [1, 4, 12, 65, 45, 12, 76, 12, 23, 42, 12]
+# # # s=0
+# # #
+# # # for b in py_list:
+# # #     if b==12:
+# # #         s+=1
+# # # print(b)
+# #
+# # # a = int(input('togri javob: '))
+# # # b = int(input('notogri javob: '))
+# # # a = a * 5
+# # # b = b * 2
+# # # c = a - b
+# # # print(c)
+# #
+# #
+# # # masofa = 310
+# # # rasxod = 5
+# # # Bakda = 30
+# # #
+# # # litr = masofa / rasxod
+# # #
+# # # kerak = litr - Bakda
+# # #
+# # # a = 9
+# # # b = float(input('kelgan vaqti'))
+# # #
+# # # if a >= b:
+# # #     print('vaqtida keldi')
+# # # else:
+# # #     a < b
+# # #     print('kechga qoldi')
+# #
+# #
+# #
+# # # a = int(input(' son kiriting '))
+# # #
+# # # b = a // 1000
+# # # c = a // 100 % 10
+# # # d = a % 100 // 10
+# # # e = a % 10
+# # #
+# # # son = e*1000+d*100+c*10+b*1
+# # #
+# # # print(son, 'teskarisi')
+# #
+# #
+# # # umumiy = 70000 * 6
+# # # usta_xaqqi = umumiy * 35 / 100
+# # # kompaniya = umumiy - usta_xaqqi
+# # #
+# # # print(kompaniya, usta_xaqqi)
+# #
+# # # a = int(input('3 xonali son kiriting'))
+# #
+# #
+# # # b = a // 100
+# # # c = a // 10 % 10
+# # # d = a % 10
+# # # e = b*100+c*10+d*1
+# # #
+# # # if e == a:
+# # #     print("true")
+# # # else:
+# # #     print('False')
+# #
+# #
+# #
+# # # a = 2
+# # # g = 0
+# # #
+# # # s = int(input(' kelgan soat '))
+# # # b = int(input('kelgan minut '))
+# # #
+# # # if a >= s and b >= g:
+# # #     print(f'{a} soat {g} minut kechikdi')
+# # # elif s <= a and b > g:
+# # #     print('vaqtida keldi')
+# # # elif s == a and b >= g:
+# # #     print('vaqtida keldi')
+# # #
+# # # elif s <= g and b > g:
+# # #     print('vaqtida keldi')
+# # #
+# # # else:
+# # #     ...
+# #
+# # # list = [9, 6, 17, 24, 28, 101, 238, 457, 496, 6452, 8128]
+# # # a=1
+# # # for i in list:
+# # #     for a in range (1,i):
+# # #         if i % a == 0:
+# # #             print(i)
+# # #         else:
+# #
+# # # a = "menen seni oldiraman deganimga kop bolgan shunga"
+# # # print (a.count('e'))
+# #
+# #
+# # # def my_list(a):
+# # #     empty=[]
+# # #     for char in my_list:
+# # #         empty.append(char.upper())
+# # #     return empty
+# # # list = [string]
+# # # print(my_list(list))
+# #
+# # # def multiply_even(my_list):
+# # #     product = 1
+# # #     for number in my_list:
+# # #         if number % 2 == 0:
+# # #             product *= number
+# # #     return product
+# #
+# #
+# # # my_list = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+# # # result = multiply_even(my_list)
+# # # print("Juft sonlarning ko'paytmasi:", result)
+# #
+# #
+# #
+# # # def find_max(numbers: list) -> int:
+# # #
+# # #     maxx = numbers[0]
+# # #     for number in numbers:
+# # #         if number > maxx:
+# # #             maxx = number
+# # #
+# # #     return maxx
+# # #
+# # #
+# # #
+# # # lst = [3, 6, 50, 2, 1, 7, 24, 21]
+# # #
+# # #
+# # # # print(lst[1])
+# # #
+# # # result = find_max(lst)
+# # # print(result)
+# #
+# #
+# # ################################################
+# #
+# # # def my_function(lst: list):
+# # #     empty = []
+# # #     for a in lst:
+# # #         if len(a) > 5:
+# # #             empty.append(a)
+# # #     return empty
+# # #
+# # #
+# # # strings = ['hi', 'hello', 'something', 'another', 'sdfdsf', 'ab', 'abc']
+# # #
+# # # res = my_function(strings)  # ['something', 'another', 'sdfdsf']
+# # #
+# # # print(res)
+# #
+# # ################################################
+# #
+# # # def delete_empty_strings(strings: list):
+# # #     empty = []
+# # #     for string in strings:
+# # #         if string != '':
+# # #             empty.append(string)
+# # #
+# # #     return empty
+# # #
+# # #
+# # # list1 = ["Mike", "", "Emma", "Kelly", "", "Brad"]
+# # # b = delete_empty_strings(list1)
+# # #
+# # # print(b)
+# # #
+# # # natija: ["Mike", "Emma", "Kelly", "Brad"]
+# #
+# #
+# # ################################################
+# #
+# # # def calculate_cube(numbers: list):
+# # #     empty = []
+# # #
+# # #     for n in numbers:
+# # #         c = n ** 3
+# # #         print(c)
+# # #         empty.append(n ** 3)
+# # #
+# # #     return empty
+# # #
+# # #
+# # # lst = [2, 4, 3, 5, 6]
+# # # # lst = list(range(7))
+# # # a = calculate_cube(lst)
+# # # print(a)
+# #
+# # # list ['abc', 'hi', 'hello']
+# # #
+# # # def str_upper(list):
+# # #     return list.upper()
+# #
+# #
+# # #
+# # # yengi_list = [empty for empty in range(1, 1001) if empty % 7 == 0]
+# # # print(sum(yengi_list))
+# #
+# #
+# # # t = (1, 2, 3, 4, 5, 6)
+# # # t = t + (10, 5)
+# # # print (t)
+# #
+# #
+# #
+# # # lst = [4, 2, 6, 7, 8]
+# # #
+# # # list_comp = [val ** 3 for val in lst]
+# # # print(list_comp)
+# #
+# #users = {
+#
+#
+# #
+# # # lst = ['abc', 'hi', 'hello']
+# # #
+# # # def str_upper(katta):
+# # #     return katta.upper
+# # #
+# # # m = list(map(str.upper, lst))
+# # # print(m)
+# #
+# #
+# # animals = ['animals' 'DOG', 'cow', 'monkey', 'ABC']
+#
+#
+# ##############################################################
+#
+#
+# # args = [2, 3, 4, 'abc', 'cda', 'dba']
+# #
+# # # def function(*args):
+# # #     k = 1
+# # #
+# # #     for arg in args:
+# # #         if isinstance(arg, int):
+# # #             k *= arg
+# # #     return k
+# # #
+# # #
+# # # print(function(*args))
+#
+#
+#
+# # def arifmetik(num: list) -> int:
+# #
+# #
+# #     practise = [list1 for list1 in numbers if list1 % 2 == 0]
+# #     res = (sum(practise)/len(practise))
+# #     return res
+# #
+# # numbers = [6, 3, 2, 4, 9, 11]
+# # print(arifmetik(numbers))
+#
+#
+# ########################################################################################
+#
+# # def tuple_cod(atuple, element):
+# #     if element not in atuple:
+# #         return f"{element} tuple da mavjud emas"
+# #     else:
+# #         lst = list(atuple)
+# #         lst.remove(element)
+# #         return tuple(lst)
+# #
+# #
+# # a = (12, 23, 45, 56, 78, 66)
+# # b = 12
+# # print(tuple_cod(a,b))
+#
+# # another = 'lorem ipsum is simply dummy'
+# # print(another.split())
+# # a = another.split()
+# # print(' '.join(a))
+#
+#
+#
+# # empty = dict()
+# # empty = {}
+#
+# #
+# # users = {
+# #     "Alice": "alice123#",
+# #     "Bob": "bob123",
+# #     "John": "qwerty123"
+# # }
+# #
+# # def sign_in(username, password):
+# #     if username in users:
+# #         if users[username] == password:
+# #             print("Muvaffaqiyatli kirdingiz")
+# #         else:
+# #             print("Noto'g'ri parol kiritdingiz")
+# #     else:
+# #         print("Bunday foydalanuvchi mavjud emas")
+# #
+# #
+# # username = input("Foydalanuvchi nomi: ")
+# # password = input("Parol: ")
+# #
+# # sign_in(username, password)
+#
+# # word = ['programming', 'python', 'language']
+# #
+# # visaversa = {char: char[::-1] for char in word}
+# #
+# # print(len_visaversa)
+#
+#
+# # lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# #
+# #
+# # cubes = {num:}
+#
+# # lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# #
+# # square = {num: num ** 2 for num in lst if num % 3 ==0}
+# # print(square)
+# #
+# #
+# #
+# #
+# # products = [
+# #     ('apple', 'fruit'),
+# #     ('banana', 'fruit'),
+# #     ('melon', 'vegetables'),
+# #     ('watermelon', 'vegetables'),
+# # ]
+# #
+# #
+# # student_grades = {
+# # "Alice": {"Math": 85, "Science": 90},
+# # "Bob": {"Math": 78, "Science": 82},
+# # }
+# #
+# # new_dict = {}
+# # for val in student_grades:
+# #
+# #     new_dict[student] =
+#
+#
+# # expenses = {
+# # "January": {"Rent": 1200, "Groceries": 300, "Utilities": 150},
+# # "February": {"Rent": 1200, "Groceries": 320, "Utilities": 180},
+# # }
+# #
+# # new_dict = {}
+# #
+# # for val in expenses:
+# #     exp = expenses[val].values()
+# #
+# #     new_dict[val] = sum(exp)
+# #     print(new_dict)
+#
+#
+#
+# # fruits = ["Apple", "Banana", "Orange", "Grapes"]
+# #
+# # for index, fruit in enumerate(fruits):
+# #     print("Index:", index, "Fruit:", fruit)
+#
+#
+# # num = [1, 2, 2, 3, 4, 4, 5]
+# #
+# #
+# # a = set(num)
+# # print(a)
+#
+#
+# # list1 = ["apple", "banana", "cherry"]
+# # list2 = ["banana", "cherry", "date"]
+# #
+# #
+# # print(list(set(list1) & set(list2)
+# #
+# #
+# # employees = {
+# #     "Alice": {"Python", "Java"},
+# #     "Bob": {"JavaScript", "HTML"},
+# #     "Charlie": {"Python", "JavaScript"}
+# # }
+# #
+# #
+# # emp = input('ischi: ')
+# # skill = input('mahorat: ')
+# #
+# #
+# # # a = employees
+# #
+# #
+# # # word = "Never skip me"
+# # # #
+# # # splitting = word.split(" ")
+# # #
+# # #
+# # # len_string = {count: len(count) for count in splitting}
+# # # print(len_string)
+# #
+# # #
+# # empty = []
+# # my_set = [1, 2, 3, 4]
+# # set2 = [4, 3, 9, 0]
+# # #
+# # # print(set(my_set) - set(set2))
+# # # print(set(my_set) & set(set2))
+# #
+# # for item in my_set:
+# #     if item in set2:
+# #         empty.append(item)
+# # #
+# # #         print(empty)
+# # #
+# # def umumiy_xarf(list1, list2):
+# #     for item in list1:
+# #         if item in list2:
+# #             return True
+# #     return False
+# #
+# #
+# #
+# # list1 = [1, 2, 3, 4, 5]
+# # list2 = [5, 6, 7, 8, 9]
+# # print(umumiy_xarf(list1, list2))
+#
+# #######################################################  EXAM TEST ######################################################
+#
+#
+# numbers = [1, 2, 3, "xariflar", "sozlar", 4, 5, 6, 7, 8]
+# def function_test(*args):
+#     integer_soni = 0
+#     string_soni = 0
+#
+#     for arg in args:
+#         if isinstance(arg, int):
+#             integer_soni += 1
+#         elif isinstance(arg, str):
+#             string_soni += 1
+#
+#     return integer_soni, string_soni
+# print(function_test(*numbers))
+#
+# #######################################################  EXAM TEST 2 DICTIONARY ######################################################
+#
+# ######  1  ##########
+# # reports = [
+# #     (100000, 'Income'),
+# #     (100000, 'Outcome'),
+# #     (120000, 'Income'),
+# # ]
+# # my_dict = {}
+# #
+# # ### 1
+# # for items in reports:
+# #     if items[1] in my_dict:
+# #         my_dict[items[1]].append(items[0])
+# #     else:
+# #         my_dict[items[1]] = [items[0]]
+# # print(my_dict)
+# #
+# #
+# # ### 2
+# # def add_income(income):
+# #     my_dict['Income'].append(income)
+# #
+# #
+# # income = int(input('Income kiriting: '))
+# # print(add_income(income))
+# # print(my_dict)
+#
+#
+# ### 3
+# # def add_income(outcome):
+# #     my_dict['Outcome'].append(outcome)
+# #
+# #
+# # outcome = int(input('Outcome kiriting: '))
+# # print(add_income(outcome))
+# # print(my_dict)
+# #
+# #
+# # ### 4
+# # def calculate_report():
+# #     i = sum(my_dict['Income'])
+# #     o = sum(my_dict['Outcome'])
+# #     return i - o
+# #
+# #
+# # print(calculate_report())
+#
+
+
+
+
+
+
+import os
+import csv
+
+
+
+with open('contacts.csv', 'w', encoding='utf-8') as all_infos:
+    full_name = input("full name kiriting: ")
+    password = input("password: ")
+    email = input("mail kiriting: ")
+    all_infos.write(f"{full_name}| {password}| {email}\n")
+
+
+def pass_check(password):
+    if not len(password) >= 8:
+        print("must be more than 8 digits")
+        return adding_new()
+    elif password.isdigit():
+        print("incorrect")
+        return adding_new()
+
+
+def pass_checking(password):
+    with open('contacts.csv', 'r', encoding='utf-8') as file_reading:
+        x = csv.reader(file_reading, delimiter='|')
+        for num in x:
+            if x[1].lstrip() == password:
+                print("Done")
+
+def email_check(email):
+    if not "@mail" in email:
+        print("invalid email")
+        return adding_new()
+
+
+
+def email_checking(email):
+    with open('contacts.csv', 'r', encoding='utf-8') as file_reading:
+        x = csv.reader(file_reading, delimiter='|')
+        for nums in x:
+            if x[2].lstrip() == email:
+                print("Done")
+
+def adding_new():
+    password = input("Pass: ")
+    pass_check(password)
+    email = input("email: ")
+    email_check(email)
+    full_name = input("Full name: ")
+    with open('contacts.csv', 'a') as all_infos:
+        all_infos.write((f"{full_name}| {password}| {email}\n")
+
+
+
+
